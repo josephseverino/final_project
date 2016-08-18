ccf0987132bdbb1d1b1307f2521668f2d8c9a897
Student <student@Admins-Mac-mini-3.local>
2016-08-18 17:04:32 -0600

    made my landing page

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
 require('colors');
 
-var express    = require('express'),
-    logger     = require('morgan'),
+var express = require('express'),
     bodyParser = require('body-parser'),
-    path       = require('path'),
-    mongoose   = require('mongoose'),
-    ejs        = require('ejs'),
-    sessions   = require('client-sessions'),
-    port       = process.env.PORT || 3000,
-    Routes     = require('./routes')
-    app        = express();
+    path = require('path'),
+    logger = require('morgan'),
+    ejs = require('ejs'),
+    mongoose = require('mongoose'),
+    sessions = require('client-sessions'), // encrypted cookies!
+    port = process.env.PORT || 1337,
+    Routes = require('./routes'),
+    app = express();
 
-mongoose.connect('mongodb://localhost/gear-to-share', (err)=> {
-    if(err){
-        console.error('mongodb is having problems'.red, err)
-    }else{
-        console.info('mongodb is working!'.green)
-    }
-});
-
-app.use( logger('dev'));
+app.use(logger('dev'));
 app.use(sessions({
     cookieName: '_mean-auth', // front-end cookie name
-    secret: 'a9iysc', // the encryption password : keep this safe
+    secret: 'a9iysc', // the encryption password : keep this safe DR@G0N$
     requestKey: 'session', // req.session,
     duration: 86400, // 60 * 60 * 24 (number of seconds in a day), tells the middleware when the cookie/session should expire,
     cookie: {
@@ -32,9 +24,8 @@ app.use(sessions({
     }
 }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use( bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'public')));
+app.post('*', bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine','html'); // allows us to specify the default extension for the files in the views folder
 app.engine('html', ejs.renderFile); // this is the function that binds to res.render
@@ -47,10 +38,12 @@ mongoose.connect('mongodb://localhost/mean-auth', (mongooseErr) => {
     }
 });
 
-app.listen(port, (err) =>{
-    if(err){
-        console.error('server is not working: '.red, err)
-    }else{
-        console.info('server is up and running!!'.cyan, port)
+Routes(app);
+
+app.listen(port, (err) => {
+    if( err ) {
+        console.error('#ERROR#'.red,'Could not start server! :(');
+    } else {
+        console.log('\nMEAN Auth Server UP!'.green.bold, 'PORT:'.yellow, port);
     }
 })
