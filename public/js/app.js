942eba2effef006cd81bfbc73f84e851199a0ed7
Student <student@Admins-Mac-mini-3.local>
2016-08-23 17:09:47 -0600

    Put in google maps and alltrails using jquery

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,40 @@
-angular.module('Gear',[])
-    .controller('Profiles',Profiles)
+ngular.module('GearShare',["ngRoute"])
 
-function Profiles(){
-    var profiles = this;
-    profiles.login =false;
+
+angular.module('GearShare').config(function($routeProvider){
+    $routeProvider.when('/home', {
+        templateUrl: './views/home.html'
+    });
+
+    $routeProvider.when('/loan', {
+        templateUrl: './views/loan.html'
+    });
+    $routeProvider.when('/rent', {
+        templateUrl:'./views/rent.html'
+    });
+    $routeProvider.when('/near', {
+        templateUrl:'./views/near.html'
+    });
+
+    $routeProvider
+        .otherwise({
+            redirectTo:'/home'
+        });
+});
+
+angular.module('GearShare').controller('shareCtrl', ['$scope', '$location', shareCtrl])
+function shareCtrl( $scope, $location ) {
+    var  sCtrl = this;
+
+    var foo = '' ;
+    $scope.$on('$routeChangeSuccess', function() {
+        sCtrl.locationPath = $location.path();
+        console.log('locationPath: ' + sCtrl.locationPath );
+    });
 }
+
+angular.module('GearShare').controller('aboutCtrl', function ($scope) {
+    $scope.aboutText = 'Every page on this site is embedded inside the index.html';
+});
+
+console.log('before my new controller and factory')
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,12 @@ module.exports = (app) => {
         // res.redirect('/login'); // I don't have a landing page, so just redirect to the login page!
         res.render('landingPage.html')
     });
+    app.get('/search', (req,res) => {
+        res.render('search.html')
+    });
+    app.get('/explore', (req,res) => {
+        res.render('explore.html')
+    });
 
     app.get('/login', Auth.render)  // login page
     app.get('/logout', Auth.logout) // logout route + redirect
